92de6b74a0621cfe4e7cf05ad6f5bcdb7c8fbee3
hilgayaa <hilgayaa@gmail.com>
2025-11-25 02:49:03 +0530

    let me check

--- lib/types.ts
+++ lib/types.ts
@@ -1,13 +1,5 @@
-import { Product, Category, Inquiry } from '@prisma/client'
+// import { Product, Category, Inquiry } from '@prisma/client'
 
-export type ProductWithCategory = Product & {
-  category: Category
-}
-
-export type CategoryWithProducts = Category & {
-  products: Product[]
-  children?: Category[]
-}
 
 export type SearchFilters = {
   category?: string
